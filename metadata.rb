21ccee0347be9a01a47b4b8e737fcdc32e3eb9da
lamont <lamont@fastrobot.com>
2019-11-14 00:03:14 -0800

    foodcritic fixes

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,16 @@
 name 'frPlex'
 maintainer 'Lamont Lucas'
 maintainer_email 'lamont@fastrobot.com'
-license 'all_rights'
+license 'Apache-2.0'
 description 'Installs/Configures frPlex'
 long_description 'Installs/Configures frPlex'
 version '0.2.3'
 
+issues_url 'https://github.com/FastRobot/frPlex/issues'
+source_url 'https://github.com/FastRobot/frPlex'
+
+chef_version '>= 12'
+%w( debian ubuntu ).each do |os|
+  supports os
+end
 depends 'docker', '~> 4.8'
