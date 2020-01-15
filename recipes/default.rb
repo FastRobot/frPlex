04a09cbb070981686f908db9029b4984b74c21da
lamont <lamont@fastrobot.com>
2020-01-15 08:15:54 -0800

    cookstyle

--- recipes/default.rb
+++ recipes/default.rb
@@ -10,11 +10,11 @@ package 'nfs-common'
 directory '/media'
 
 mount '/media' do
- device node['frPlex']['media']['nfs']
- fstype 'nfs'
- options 'rw'
- action [:mount, :enable]
- only_if node['frPlex']['mount_nfs']
+  device node['frPlex']['media']['nfs']
+  fstype 'nfs'
+  options 'rw'
+  action [:mount, :enable]
+  only_if node['frPlex']['mount_nfs']
 end
 
 directory '/etc/systemd/system/docker.service.d' do
@@ -31,21 +31,20 @@ end
 
 # setup zfs
 
- package 'zfsutils-linux'
- # I manually ran build@docker1:~$ sudo zpool create tank vdb vdc -f
+package 'zfsutils-linux'
+# I manually ran build@docker1:~$ sudo zpool create tank vdb vdc -f
 
- execute 'zpool create tank vdb vdc -f' do
-   not_if "zpool list | grep tank"
-   only_if node['frPlex']['manage_zfs']
- end
+execute 'zpool create tank vdb vdc -f' do
+  not_if 'zpool list | grep tank'
+  only_if node['frPlex']['manage_zfs']
+end
 
- execute 'zfs create -o mountpoint=/var/lib/docker tank/docker' do
-   returns [0]
-   action :run
-   not_if "zfs list | grep docker"
-   only_if node['frPlex']['manage_zfs']
- end
-
+execute 'zfs create -o mountpoint=/var/lib/docker tank/docker' do
+  returns [0]
+  action :run
+  not_if 'zfs list | grep docker'
+  only_if node['frPlex']['manage_zfs']
+end
 
 # configure docker
 package %w(apt-transport-https ca-certificates tmux)
@@ -53,7 +52,7 @@ package %w(apt-transport-https ca-certificates tmux)
 execute 'add docker repo keys' do
   command 'apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D'
   action :run
-  not_if "apt-key list | grep releasedocker"
+  not_if 'apt-key list | grep releasedocker'
 end
 
 file '/etc/apt/sources.list.d/docker.list' do
@@ -94,17 +93,16 @@ docker_container 'plex' do
   restart_policy 'always'
   network_mode 'host'
   volumes %w(plex-config:/config
-           plex-transcode:/transcode
-           /media/Movies:/data/movies
-           /media/TV:/data/tvshows
-           /media/HomeMovies:/data/homemovies
-           /media/Music:/data/music
-           /media/Cartoons:/data/cartoons)
+             plex-transcode:/transcode
+             /media/Movies:/data/movies
+             /media/TV:/data/tvshows
+             /media/HomeMovies:/data/homemovies
+             /media/Music:/data/music
+             /media/Cartoons:/data/cartoons)
 end
 
 # monitor it
 docker_image 'google/cadvisor'
-
 
 docker_container 'cadvisor' do
   repo 'google/cadvisor'
@@ -116,11 +114,8 @@ docker_container 'cadvisor' do
     /var/run:/var/run:rw
     /sys:/sys:ro
     /var/lib/docker/:/var/lib/docker:ro
-    )
+  )
 end
-
-
-
 
 # plexpy
 
@@ -132,4 +127,3 @@ end
 # /var/lib/docker/.zfs/snapshot/20160925/volumes/plex-config/_data
 
 # root@docker1 /v/l/d/.z/s/2/v/p/_data# rsync -a --progress --exclude=Cache/ Library user@somewhere:tmp/
-
