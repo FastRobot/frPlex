2d0d2866383fa8bb443ea9a81a3072dff3f2fb03
lamont <lamont@fastrobot.com>
2021-08-15 14:14:08 -0700

    typo.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
 default['frPlex']['media']['nfs'] = 'some.lan.host:/exported/media'
 default['frPlex']['mount_nfs'] = true
 default['frPlex']['manage_zfs'] = true
-default['frPlex']['plex_claim'] - nil
+default['frPlex']['plex_claim'] = nil
